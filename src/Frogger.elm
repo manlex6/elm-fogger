016a9f1955fcbe40aa89e77c7399d28415c5f4e2
manlex6 <manlex6@gmail.com>
2017-02-13 07:21:27 -0500

    Setup proyect

---
+++ src/Frogger.elm
@@ -0,0 +1,35 @@
+module Frogger exposing(..)
+
+import Html exposing(..)
+
+type Msg 
+    = NoOp
+
+type alias Model = Int
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+  case msg of
+    NoOp ->
+      ( model, Cmd.none )
+  
+view : Model -> Html Msg
+view model =
+    div [] [ text "Hello Frogger" ]
+
+
+main : Program Never Model Msg
+main =
+  Html.program
+    { init = (0, Cmd.none)
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+
+      
